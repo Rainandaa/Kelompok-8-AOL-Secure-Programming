531d3a3d08917c11df196e5cf2576b8cf1ce10f4
rai <107031034+Rainandaa@users.noreply.github.com>
2024-01-06 17:40:16 +0700

    Update update_profile_process.php

--- controllers/update_profile_process.php
+++ controllers/update_profile_process.php
@@ -1,38 +1,37 @@
 <?php
 session_start();
-require "./Connection.php"; // Tambahkan semikolon di sini
+require "./Connection.php"; 
 
-// Pastikan pengguna telah login
 if ($_SESSION['is_login'] !== true) {
     header("Location: login.php");
-    exit(); // Pastikan untuk keluar dari skrip setelah melakukan redirect
+    exit();
 }
 
-// Mendapatkan data dari formulir
-$email = $_POST['email'];
-$nama_asli = $_POST['nama_asli'];
-$tempat_lahir = $_POST['tempat_lahir'];
-$tanggal_lahir = $_POST['tanggal_lahir'];
+if ($_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+    die("CSRF token validation failed.");
+}else{
+    $email = $_POST['email'];
+    $nama_asli = $_POST['nama_asli'];
+    $tempat_lahir = $_POST['tempat_lahir'];
+    $tanggal_lahir = $_POST['tanggal_lahir'];
 
-// Menghindari SQL injection dengan menggunakan prepared statement
-$query = $conn->prepare("INSERT INTO user_info (email, nama_asli, tempat_lahir, tanggal_lahir) VALUES (?, ?, ?, ?)");
+    $query = $conn->prepare("INSERT INTO user_info (email, nama_asli, tempat_lahir, tanggal_lahir) VALUES (?, ?, ?, ?)");
 
-// Bind parameter ke statement
-$query->bind_param("ssss", $email, $nama_asli, $tempat_lahir, $tanggal_lahir);
+    $query->bind_param("ssss", $email, $nama_asli, $tempat_lahir, $tanggal_lahir);
 
-// Eksekusi statement
-if ($query->execute()) {
-    $_SESSION['email'] = $email;
-    $_SESSION['nama_asli'] = $nama_asli;
-    $_SESSION['tempat_lahir'] = $tempat_lahir;
-    $_SESSION['tanggal_lahir'] = $tanggal_lahir;
+    if ($query->execute()) {
+        $_SESSION['email'] = $email;
+        $_SESSION['nama_asli'] = $nama_asli;
+        $_SESSION['tempat_lahir'] = $tempat_lahir;
+        $_SESSION['tanggal_lahir'] = $tanggal_lahir;
 
-    header("Location: ../profile.php");
-    exit(); // Pastikan untuk keluar dari skrip setelah melakukan redirect
-} else {
-    die("Error: " . $query->error);
+        header("Location: ../profile.php");
+        exit();
+    } else {
+        die("Error: " . $query->error);
+    }
 }
 
-// Menutup koneksi database
+
 $conn->close();
 ?>
